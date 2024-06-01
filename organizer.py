7ff11603449928d75c6250822fdff2839aba7524
Magnymbus <tybuddy11@gmail.com>
2024-06-01 02:11:11 -0600

    Refactored to improve clarity and performance

--- organizer.py
+++ organizer.py
@@ -1,61 +1,36 @@
 import os
 import re
-import shutil
+from glob import iglob
 
-def list_files_recursive(path='.'):
-    files = []
-    for entry in os.listdir(path):
-        full_path = os.path.join(path, entry)
-        if os.path.isdir(full_path):
-            files.extend(list_files_recursive(full_path))
-        else:
-            if entry.endswith('.cs'):
-                files.append(full_path)
-    return files
+SCRIPT_PATH_RE = re.compile(r'ScriptPath\("res://(.*?\.cs)')
 
-def getScriptPath(file):
-    with open(file, "r") as f:
-        contents = f.read()
-        repl = re.compile(r'ScriptPath\("res://(.*?\.cs)')
-        match = re.search(repl, contents)
-        if match:
-            return match.group(1)
+def get_script_path(file_path):
+    with open(file_path, "r") as f:
+        for line in f:
+            if match := SCRIPT_PATH_RE.search(line):
+                return match.group(1)
+    return None
 
-def validatePath(file):
-    dirs = file.split('\\')
-    newPath = ''
-    for dir in dirs:
-        if dir == ".":
-            continue
-        elif dir == "Game":
-            newPath = newPath + 'cripts/'
-        elif dir.endswith('.cs'):
-            newPath = newPath + dir
-        else:
-            newPath = newPath + dir.lower()
-            if not os.path.exists(newPath):
-                trypath = newPath.lower()
-                if trypath.endswith('y'):
-                    trypath = trypath[:-1]+"ies/"
-                else:
-                    trypath = trypath + "s/"
-                if not os.path.exists(trypath):
-                    trypath = newPath.lower() + '/'
-                    if not os.path.exists(trypath):
-                        raise ValueError("bad path: ", trypath)
-                    else:
-                        newPath = trypath
-                else:
-                    newPath = trypath
-            else:
-                newPath = newPath + '/'
-    return newPath
+def pluralize(string):
+    return f'{string[:-1]}ies' if string.endswith('y') else f'{string}s'
 
-directory_path = ".\\Game"
-files = list_files_recursive(directory_path)
+def validate_path(file_path):
+    parts = file_path.split('\\')
+    file_name = parts[-1]
+    valid_path = ''
+    for part in parts[:-1]:
+        if part == 'Game':
+            valid_path = os.path.join(valid_path, 'scripts')
+        elif part != '.':
+            potential_path = os.path.join(valid_path, part)
+            if not os.path.exists(potential_path):
+                potential_path = os.path.join(valid_path, pluralize(part))
+                if not os.path.exists(potential_path):
+                    raise ValueError("bad path: ", potential_path)
+            valid_path = potential_path
+    return os.path.join(valid_path.lower(), file_name)
+
+files = list(iglob(os.path.join(".\\Game", '**', '*.cs'), recursive=True))
 for file in files:
-    newPath = getScriptPath(file)
-    if not newPath:
-        newPath = validatePath(file)
-    if not os.path.exists(newPath):
-        print(newPath, " DOES NOT EXIST!")
+    new_path = get_script_path(file) or validate_path(file)
+    print(f"{new_path} {'replaces existing!' if os.path.exists(new_path) else 'DOES NOT EXIST!'}")
